fecbe03c2db0b582bd95659fcee78f378b17bf00
Enej Licina <enej.licina@windowslive.com>
2026-01-12 16:37:24 +0100

    bumping the version number and build

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "SloGas",
     "slug": "slogas",
-    "version": "1.1.0", 
+    "version": "1.2.0", 
     "orientation": "portrait",
     "icon": "./assets/app-icon-ios.png",
     "splash": {
@@ -18,7 +18,7 @@ export default {
       "bundleIdentifier": "com.enejlicina.slogas",
       "deploymentTarget": "15.1",
       "associatedDomains": ["applinks:verify.enejlicina.com"],
-      "buildNumber": "4",
+      "buildNumber": "5",
       "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       },
